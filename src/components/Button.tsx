c50eba61b7cbf8f583f46ae2e02dd791d8cdce75
Ulises Villanueva <ulisespcv@gmail.com>
2022-08-06 17:41:27 -0500

    feat: finish layout

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,27 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import { styles } from '../theme/appTheme'
 
-interface Props{
-    text:string;
+interface Props {
+    text: string;
     color?: string;
     width?: boolean;
 }
-export const Button = ({ text, color='#2D2D2D', width=false}:Props) => {
-  return (
-    <View style={{
-        ...styles.button,
-        backgroundColor:color,
-        width: (width)?  160 : 80,
-    }}>
-    <Text style={{
-        ...styles.buttonText, 
-        color: (color === '#9B9B9B')
- ? 'black':'white'    }}>{text}</Text>
-</View>
-  )
+export const Button = ({ text, color = '#2D2D2D', width = false }: Props) => {
+    return (
+        <TouchableOpacity >
+            <View style={{
+                ...styles.button,
+                backgroundColor: color,
+                width: (width) ? 160 : 80,
+            }}>
+                <Text style={{
+                    ...styles.buttonText,
+                    color: (color === '#9B9B9B')
+                        ? 'black' : 'white'
+                }}>{text}</Text>
+            </View>
+        </TouchableOpacity>
+
+    )
 }
